a3b95d78196d3a018fae3e4536f84d596468c2fc
Crowdbotics <team@crowdbotics.com>
2023-02-09 01:27:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quote-calculator-2-38823",
+  url: "https://quote-calculator-2-38823.botics.co",
+  api: "https://quote-calculator-2-38823.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
